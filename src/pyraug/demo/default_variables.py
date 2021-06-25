ad9d4738e0eb8adbf109f30f2ac2dc20df0d3e30
clementchadebec <clement.chadebec@orange.fr>
2021-06-25 11:55:28 +0200

    remove old demo

--- src/pyraug/demo/default_variables.py
+++
@@ -1,7 +0,0 @@
-PATH_TO_DEFAULT_MODEL_CONFIG = "demo/configs_files/model_config.json"
-PATH_TO_DEFAULT_TRAINING_CONFIG = "demo/configs_files/training_config.json"
-PATH_TO_DEFAULT_GENERATION_CONFIG = "demo/configs_files/generation_config.json"
-
-NEEDED_FILES_FOR_MODEL_LOADING = ["model_config.json", "model.pt"]
-
-NEEDED_KEYS_FOR_RHVAE_WEIGHTS_LOADING = ["model_state_dict", "M", "centroids"]
