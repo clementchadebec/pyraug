c0a3deed4995df70c1927c076daeaf446740b04c
clementchadebec <clement.chadebec@orange.fr>
2021-09-03 11:37:55 +0200

    change setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyraug",
-    version="0.0.2",
+    version="0.0.3",
     author="Clement Chadebec (HekA team INRIA)",
     author_email="clement.chadebec@inria.fr",
     description="Data Augmentation with VAE",
